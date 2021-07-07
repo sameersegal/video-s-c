bcbaeb96449b6e7f88bfc7a1ab37268e12ef155b
Sameer Segal <sameer@artoo.in>
2021-07-07 07:30:45 +0530

    Added codecs that work with GPU. Added loudnormalization for step videos

--- script/generate.sh
+++ script/generate.sh
@@ -14,6 +14,7 @@ parse_template() {
     count=0
     steps=()
     while IFS=, read -r type duration; do
+        # echo "$type-$duration"
         step=($type $duration)
         [[ $count != 0 ]] && steps+=($step)
         ((++count))
@@ -35,9 +36,12 @@ create_step_video(){
     ffmpeg -hide_banner -stream_loop -1 -i $input \
     -ss 00:00:00 -t $duration \
     -vf "\
+    scale=1280:720:force_original_aspect_ratio=decrease,setdar=1280/720,setsar=1/1,fps=fps=30, \
     drawtext=fontfile=/System/Library/Fonts/Monaco.dfont:text='%{eif\:$duration-t\:d}':fontcolor=white:fontsize=192:x=(w-tw-50):y=(h-th-50):box=1:boxcolor=black@0.75:boxborderw=10,\
     format=yuv420p" \
-    -c:a aac -c:v libx264 -crf 23 \
+    -af "loudnorm=i=-24:tp=-2:lra=7" \
+    -pix_fmt yuv420p -color_range 2 -movflags +write_colr \
+    -c:a aac -c:v h264_videotoolbox -b:v 1M \
     -y "$path"
 }
 
@@ -49,14 +53,16 @@ create_rest_video() {
     # TODO - add voice over & metronome 
 
     ffmpeg -hide_banner -f lavfi \
-    -i color=c=red:s=1024x678 \
+    -i color=c=red:s=1280x720 \
     -ss 00:00:00 -t $duration \
     -vf "\
+    setdar=1280/720,setsar=1/1,fps=fps=30, \
     drawtext=fontfile=/System/Library/Fonts/Monaco.dfont:text='Rest':fontcolor=white:fontsize=256:x=(w-tw)/2:y=(h-th*2)/2,\
     drawtext=fontfile=/System/Library/Fonts/Monaco.dfont:text='Next\: $label':fontcolor=white:fontsize=64:x=(w-tw)/2:y=(h/2+100),\
     drawtext=fontfile=/System/Library/Fonts/Monaco.dfont:text='%{eif\:$duration-t\:d}':fontcolor=white:fontsize=64:x=(w-tw)/2:y=(h-150),\
     format=yuv420p" \
-    -c:a aac -c:v libx264 -crf 23 \
+    -pix_fmt yuv420p -color_range 2 -movflags +write_colr \
+    -c:a aac -c:v h264_videotoolbox -b:v 1M \
     -y "$path"
 }
 
@@ -69,7 +75,10 @@ concatenate() {
         [[ $count != 0 ]] && input="$input|$video"
         ((++count))
     done
-    ffmpeg -hide_banner -y -i "$input" -c:a aac -c:v libx264 -crf 23 "$staging/$final"
+    ffmpeg -hide_banner -y -i "$input" \
+    -c:a aac -c:v h264_videotoolbox -b:v 1M -vsync 2 \
+    -pix_fmt yuv420p -color_range 2 -movflags +write_colr \
+    "$staging/$final"
     # ffplay -hide_banner -i "$input"
 }
 
@@ -87,7 +96,7 @@ concatenate() {
 
     # concat files
 
-# create_rest_video "$staging/rest-3-barbell-squats.ts" 3 "Barbell Squats"
-# create_step_video "$staging/barbell-squats-3-deep.ts" 120 "$library/barbell-squats/deep.mp4"
+create_rest_video "$staging/rest-3-barbell-squats.ts" 3 "Barbell Squats"
+create_step_video "$staging/barbell-squats-3-deep.ts" 5 "$library/barbell-squats/deep.mp4"
 # parse_template $template
-# concatenate
+concatenate
