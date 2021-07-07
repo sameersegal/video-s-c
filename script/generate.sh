9f6de0d13b179a7fb872dd6bfc89626daf49faa9
Sameer Segal <sameer@artoo.in>
2021-07-07 22:40:03 +0530

    Got nested arrays to work with help from a stackoverflow answer

--- script/generate.sh
+++ script/generate.sh
@@ -10,33 +10,88 @@ final="${final%.*}.mp4"
 declare -a PARAMS
 declare -a VIDEOS
 
+# Nested Arrays copied from https://stackoverflow.com/a/67549770/356580
+
+# Convert an array to a string that can be used to reform 
+# the array as a new variable. This allows functions to
+# return arrays as strings. Works for arrays and associative
+# arrays. Spaces and odd characters are all handled by bash 
+# declare.
+# Usage: stringify variableName
+#     variableName - Name of the array variable e.g. "myArray",
+#          NOT the array contents.
+# Returns (prints) the stringified version of the array.
+# Examples. Use declare to make an array:
+#     declare -a myArray=( "O'Neal, Dan" "Kim, Mary Ann" )
+# (Or to make a local variable replace declare with local.)
+# Stringify myArray:
+#     stringifiedArray="$(stringify myArray)"
+# Reform the array with any name like reformedArray:
+#     eval "$(unstringify reformedArray "$stringifiedArray")"
+# To stringify an argument list "$@", first create the array
+# with a name:     declare -a myArgs=( "$@" )
+stringify() {
+    declare -p $1
+}
+
+# Reform an array from a stringified array. Actually this prints
+# the declare command to form the new array. You need to call 
+# eval with the result to make the array.
+# Usage: eval "$(unstringify newArrayName stringifiedArray [local])"
+#     Adding the optional "local" will create a local variable 
+#     (uses local instead of declare).
+# Example to make array variable named reformedArray from 
+# stringifiedArray:
+#     eval "$(unstringify reformedArray "$stringifiedArray")"
+unstringify() {
+    local cmd="declare"
+    [ -n "$3" ] && cmd="$3"
+    # This RE pattern extracts 2 things:
+    #     1: the array type, should be "-a" or "-A"
+    #     2: stringified contents of the array 
+    # and skips "declare" and the original variable name.
+    local declareRE='^declare ([^ ]+) [^=]+=(.*)$'
+    if [[ "$2" =~ $declareRE ]]
+    then
+        printf '%s %s %s=%s\n' "$cmd" "${BASH_REMATCH[1]}" "$1" "${BASH_REMATCH[2]}"
+    else
+        echo "*** unstringify failed, invalid stringified array:" 1>&2
+        printf '%s\n' "$2" 1>&2
+        return 1
+    fi
+}
+
 parse_template() {
     template=$1
     count=0
-    steps=()
-    durations=()
+    declare -a steps
     while IFS=, read -r t d; do                
-        [[ $count != 0 ]] && steps+=($t) && durations+=($d)
+        if [ $count != 0 ]
+        then
+            declare -a step=( $t $d )
+            steps[$count]="$(stringify step)"
+        fi
         ((++count))
     done < $template
     
-    # count=0
-    # for step in ${steps[*]}; do
-    #     STEP=()
-    #     d=${durations[$count]}
-    #     nxt=${steps[$count+1]}
-    #     if [ $step == "rest" ]
-    #     then
-    #         STEP=($step "$staging/${step}-${d}-${nxt}.ts" $d)
-    #     else
-    #         STEP=($step "$staging/${step}-${d}.ts" $d)
-    #     fi
-    #     PARAMS+=(${STEP[*]})
-    #     ((++count))
-    # done
-
-    # echo "${PARAMS[@]}"
-    
+    count=0
+    for step in "${steps[@]}"; do
+        eval "$(unstringify row "$step")"
+        # echo "$count: ${row[0]} & ${row[1]}"
+        
+        t=${row[0]}
+        d=${row[1]}
+        eval "$(unstringify nxt "${steps[$count+1]}")"
+        declare -a s
+        if [ $t == "rest" ]
+        then
+            s=($t "$staging/${t}-${d}-${nxt[0]}.ts" $d)
+        else
+            s=($t "$staging/${t}-${d}.ts" $d)
+        fi
+        PARAMS[$count]="$(stringify s)"
+        ((++count))
+    done
 }
 
 create_step_video(){
@@ -111,7 +166,8 @@ main() {
     parse_template $template
 
     for step in "${PARAMS[@]}"; do
-        echo $step
+        eval "$(unstringify row "$step")"
+        echo "${row[0]} || ${row[1]} || ${row[2]}"
         # check if file "{name}.ts" exists
         # is type == rest
             # call rest-video
@@ -122,7 +178,7 @@ main() {
 
     done
 
-    concatenate
+    # concatenate
 }
 
 # create_rest_video "$staging/rest-3-barbell-squats.ts" 3 "Barbell Squats"
@@ -131,54 +187,3 @@ main() {
 #VIDEOS=("$staging/rest-3-barbell-squats.ts" "$staging/barbell-squats-3-deep.ts")
 # concatenate
 main
-
-# Nested Arrays copied from https://stackoverflow.com/a/67549770/356580
-
-# Convert an array to a string that can be used to reform 
-# the array as a new variable. This allows functions to
-# return arrays as strings. Works for arrays and associative
-# arrays. Spaces and odd characters are all handled by bash 
-# declare.
-# Usage: stringify variableName
-#     variableName - Name of the array variable e.g. "myArray",
-#          NOT the array contents.
-# Returns (prints) the stringified version of the array.
-# Examples. Use declare to make an array:
-#     declare -a myArray=( "O'Neal, Dan" "Kim, Mary Ann" )
-# (Or to make a local variable replace declare with local.)
-# Stringify myArray:
-#     stringifiedArray="$(stringify myArray)"
-# Reform the array with any name like reformedArray:
-#     eval "$(unstringify reformedArray "$stringifiedArray")"
-# To stringify an argument list "$@", first create the array
-# with a name:     declare -a myArgs=( "$@" )
-stringify() {
-    declare -p $1
-}
-
-# Reform an array from a stringified array. Actually this prints
-# the declare command to form the new array. You need to call 
-# eval with the result to make the array.
-# Usage: eval "$(unstringify newArrayName stringifiedArray [local])"
-#     Adding the optional "local" will create a local variable 
-#     (uses local instead of declare).
-# Example to make array variable named reformedArray from 
-# stringifiedArray:
-#     eval "$(unstringify reformedArray "$stringifiedArray")"
-unstringify() {
-    local cmd="declare"
-    [ -n "$3" ] && cmd="$3"
-    # This RE pattern extracts 2 things:
-    #     1: the array type, should be "-a" or "-A"
-    #     2: stringified contents of the array 
-    # and skips "declare" and the original variable name.
-    local declareRE='^declare ([^ ]+) [^=]+=(.*)$'
-    if [[ "$2" =~ $declareRE ]]
-    then
-        printf '%s %s %s=%s\n' "$cmd" "${BASH_REMATCH[1]}" "$1" "${BASH_REMATCH[2]}"
-    else
-        echo "*** unstringify failed, invalid stringified array:" 1>&2
-        printf '%s\n' "$2" 1>&2
-        return 1
-    fi
-}
